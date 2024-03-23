184bb0205feff154f73d7a21a32cb98e205bd019
Fabio Cantarani <fabio.cantarani@gmail.com>
2024-03-23 07:25:05 -0300

    feat: login page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import Home from "@/pages/home";
+import Login from "@/pages/login";
 import MasterPager from "@/pages/master-page";
 import MyRequests from "@/pages/my-requests";
 import Admin from "@/pages/settings";
@@ -6,6 +7,10 @@ import Category from "@/pages/settings/category";
 import { createBrowserRouter } from "react-router-dom";
 
 export const AppRoute = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
   {
     path: "/",
     element: <MasterPager />,
