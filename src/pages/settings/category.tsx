dc46a27d6ab27ffb249a53d15e8042ff325cded3
Fabio Cantarani <fabio.cantarani@gmail.com>
2024-03-31 22:02:22 -0300

    feat: Litle Adjustment

--- src/pages/settings/category.tsx
+++ src/pages/settings/category.tsx
@@ -1,32 +1,35 @@
-import { useEffect, useState } from "react";
-import { Category as CategoryModel } from "../../models/Category";
-import settings from "../../settings.json";
+// import { useEffect, useState } from "react";
+// import { Category as CategoryModel } from "../../models/Category";
+// import settings from "../../settings.json";
 
 export default function Category() {
-  const [categories, setCategories] = useState<CategoryModel[]>();
+  // const [categories, setCategories] = useState<CategoryModel[]>();
 
-  const baseURL = settings.apiBaseUrl ?? "";
-  const url = `${baseURL}/api/v1/Categories`;
+  // const baseURL = settings.apiBaseUrl ?? "";
+  // const url = `${baseURL}/api/v1/Categories`;
 
-  const loadCategories = () => {
-    console.log("....🔥....");
-    fetch(url, { mode: "cors" })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setCategories(data);
-      });
-  };
+  // const loadCategories = () => {
+    // console.log("....🔥....");
+    // fetch(url, { mode: "cors" })
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     setCategories(data);
+    //   });
+  // };
 
-  useEffect(() => {
-    loadCategories();
-  });
+  // useEffect(() => {
+  //   loadCategories();
+  // });
 
-  if (categories)
-    return (
-      <main className="my-4">
-        <h1>{categories.map((x) => x.name)}</h1>
-      </main>
-    );
+  // if (categories)
+  //   return (
+  //     <main className="my-4">
+  //       <h1>{categories.map((x) => x.name)}</h1>
+  //     </main>
+  //   );
+  return(
+    <>Categorias</>
+  )
 }
